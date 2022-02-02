0e4d45009c07c492c68d7a2d796e615504c0457c
Márcio Possani Marques <mpmarques@possani.com.br>
2022-02-02 18:49:04 +0000

    Arquivos iniciais do GO

---
+++ infra/repository/course_db.go
@@ -0,0 +1,29 @@
+package repository
+
+import (
+	"database/sql"
+	"github.com/mpossani/imersao-go/entity"		// go get github.com/mpossani/imersao-go/entity
+)
+
+type CourseMySQLRepository struct {
+	Db *sql.DB
+}
+
+func (c CourseMySQLRepository) Insert(course entity.Course) error {
+	stmt, err := c.Db.Prepare(`Insert into courses(id, name, description, status) values (?,?,?,?)`)
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(
+		course.ID, 
+		course.Name, 
+		course.Description, 
+		course.Status,
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
